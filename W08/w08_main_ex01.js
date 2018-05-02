544f13d85f59d0c8383eb6587baf08b3f5f1cd2a
上森　香 <1545014t@istc-a-058.epc.center.kobe-u.ac.jp>
2018-05-02 13:33:45 +0900

    Intial commit

--- W08/w08_main_ex01.js
+++ W08/w08_main_ex01.js
@@ -31,18 +31,18 @@ function main()
         [ 0, 1, 2 ], // f0
     ];
 
-    var scalars = [
+   /* var scalars = [
 	0.1,   // S0
 	0.2,   // S1
 	0.8,   // S2
-    ];
+    ]; */
 
   
     // Create a color map
     var cmap = [];
     for ( var i = 0; i < 256; i++ )
     {
-        var S = ((i-0.1)/0.7) *255.0; // [0,1]
+        var S = ((i-0.1)/0.7) * 255.0; // [0,1]
         var R = Math.max( Math.cos( ( S - 1.0 ) * Math.PI ), 0.0 );
         var G = Math.max( Math.cos( ( S - 0.5 ) * Math.PI ), 0.0 );
         var B = Math.max( Math.cos( S * Math.PI ), 0.0 );
@@ -81,7 +81,7 @@ function main()
     material.vertexColors = THREE.FaceColors;
     for ( var i = 0; i < nfaces; i++ )
     {
-	var id = faces[i];
+/*	var id = faces[i];
         var S0 = scalars[ id[0] ];
         var S1 = scalars[ id[1] ];
         var S2 = scalars[ id[2] ];
@@ -90,8 +90,8 @@ function main()
         var C2 = new THREE.Color().setHex( cmap[ S2 ][1] );
         geometry.faces[i].vertexColors.push( C0 );
         geometry.faces[i].vertexColors.push( C1 );
-        geometry.faces[i].vertexColors.push( C2 );
-      //  geometry.faces[i].color = new THREE.Color( 1, 1, 1 );
+        geometry.faces[i].vertexColors.push( C2 );   */
+       geometry.faces[i].color = new THREE.Color( 1, 1, 1 );
     }
 
     var triangle = new THREE.Mesh( geometry, material );
